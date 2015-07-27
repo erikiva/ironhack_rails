b5ca103612ee86574269c89ad44844dea9b9fd0d
natalia <nataliav@gmail.com>
2015-07-27 18:54:05 +0200

    Create Address Book project

---
+++ addressbook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Addressbook::Application.config.secret_key_base = '57eba909d4183c8c3b67f4358a1baa0cdb5d473bd7723de09c77fba0f195ab42d52c355c9d2f0bb82878e8752a378bb6979910663704a4b9959bf9b1d53a0dea'
