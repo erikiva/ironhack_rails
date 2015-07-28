6241d5b1e64993ba49236fd70a23b84ba0c97b6c
natalia <nataliav@gmail.com>
2015-07-28 17:06:50 +0200

    Initial rails structure for url shortener

---
+++ urlshortener/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urlshortener::Application.config.secret_key_base = 'fe30be3d6eeae4b9a3be4b7f4b863a7161c009ec57488d6f5c16eff1a8800c59662cc3a2b485b6ecf9a17cd3b13f521d7a00fa2ee47211d02569bbe4679dd3d6'
